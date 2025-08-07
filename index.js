0eb87fd8361862d0c8220622be10ed4516d75587
Harshad Khambe <harshadkhambe844@gmail.com>
2025-08-07 13:06:23 +0530

    token fixed of proxy

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+require('dotenv').config(); 
+
 const express = require("express");
 const axios = require("axios");
 const cors = require("cors");
@@ -7,13 +9,11 @@ app.use(cors());
 
 const PORT = process.env.PORT || 5000;
 
-const TMDB_TOKEN = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiNjg3MjYyYzhkNjk5ODBhYjI4MzJiM2UxZDYzNmNjZSIsIm5iZiI6MTczNjkwODU5NS44MSwic3ViIjoiNjc4NzFmMzNmZTI5NGEwYjQ3NGU5MTNmIiwic2NvcGVzIjpbImFwaV9yZWFkIl0sInZlcnNpb24iOjF9.g5ZUSy8YWLlDr_ksfEXZdA26IYHLGeXxJIvFXVLWurU";
+const TMDB_TOKEN = process.env.TMDB_TOKEN;
 
 app.get(/^\/api\/(.*)/, async (req, res) => {
   try {
-    // Get the matched part after /api/
     const tmdbPath = req.params[0];
-
     const queryString = new URLSearchParams(req.query).toString();
 
     const url = `https://api.themoviedb.org/3/${tmdbPath}${queryString ? '?' + queryString : ''}`;
